c0739ad9e0396bac7edb473d6df7300b14742fba
Mahmut Gundogdu <mahmutgundogdu@hotmail.com.tr>
2023-12-27 10:44:11 +0300

    Create postCreateCommand.sh

---
+++ postCreateCommand.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Install the Volo.Abp.Cli tool
+dotnet tool install -g Volo.Abp.Cli
+
+# Start SQL Server container
+docker run --name tmp-sqlserver \
+    --restart unless-stopped \
+    -d \
+    --cap-add SYS_PTRACE \
+    -e 'ACCEPT_EULA=1' \
+    -e 'MSSQL_SA_PASSWORD=myPassw0rd' \
+    -p 1433:1433 \
+    mcr.microsoft.com/azure-sql-edge
+
+# Start Redis container
+docker run --name redis \
+    -p 6379:6379 \
+    -d \
+    redis
+
+# Start RabbitMQ container
+docker run --name tmp-rabbitmq \
+    -d \
+    --restart unless-stopped \
+    -p 15672:15672 \
+    -p 5672:5672 \
+    rabbitmq:3-management
